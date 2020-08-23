410f073b971fcd40b64c0b281e0a5a955a4616e2
Andreia Souza <fsouza.andreia@gmail.com>
2020-08-23 11:30:55 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Data;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SalesWebMvc.Services
 {
@@ -24,6 +25,17 @@ namespace SalesWebMvc.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(x => x.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
